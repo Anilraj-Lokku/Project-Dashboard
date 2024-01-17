5eb8721eec84ba8be296f1e5e1eaf3814e7a7c10
Anilraj Lokku <128417620+Anilraj-Lokku@users.noreply.github.com>
2024-01-17 12:41:22 +0530

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,45 @@
+const Orders =
+[
+    {
+        productName:'HP Pavilion LAPTOP',
+        productNumber:'85631',
+        paymentStauts:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'Wooden Stand',
+        productNumber:'7745',
+        paymentStauts:'Refunded',
+        shipping:'Declined'
+    },
+    {
+        productName:'Java DSA Textbook',
+        productNumber:'36378',
+        paymentStauts:'Paid',
+        shipping:'Delivered'
+    },
+    {
+        productName:'Refrigarator',
+        productNumber:'11234',
+        paymentStauts:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'Home Utensils',
+        productNumber:'32631',
+        paymentStauts:'Paid',
+        shipping:'Delivered'
+    },
+    {
+        productName:'Gaming Pc',
+        productNumber:'98464',
+        paymentStauts:'Refunded',
+        shipping:'Declined'
+    },
+    {
+        productName:'Mens Clothing',
+        productNumber:'67664',
+        paymentStauts:'Paid',
+        shipping:'Delivered'
+    },
+]
